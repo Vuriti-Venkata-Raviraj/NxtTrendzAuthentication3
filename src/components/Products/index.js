d8c09e68eb59c7e859c89fc2e5e184de8a57f815
Vuriti-Venkata-Raviraj <vuritivenkataraviraaj@gmail.com>
2023-12-10 17:02:31 +0000

    first commit

---
+++ src/components/Products/index.js
@@ -0,0 +1,15 @@
+import Header from '../Header'
+import AllProductsSection from '../AllProductsSection'
+
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="product-sections">
+      <AllProductsSection />
+    </div>
+  </>
+)
+
+export default Products
